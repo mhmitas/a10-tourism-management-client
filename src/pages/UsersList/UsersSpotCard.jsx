6902ab5bdf13d81d201522557b89136203f927e2
Mh Mitas <mahfuzulmitas@gmail.com>
2024-04-26 23:23:34 +0600

    done delete operation

--- src/pages/UsersList/UsersSpotCard.jsx
+++ src/pages/UsersList/UsersSpotCard.jsx
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { TouristSpotsContext } from '../../provider/TouristSpotsProveder';
 
 const UsersSpotCard = ({ usersSpot }) => {
-    const { image, tourist_spot_name, country_name } = usersSpot
+    const { serverLink } = useContext(TouristSpotsContext)
+    const { image, tourist_spot_name, country_name, average_cost, short_description } = usersSpot
+    function handleDelete(id) {
+        fetch(`${serverLink}/tourist-spots/${id}`, {
+            method: 'DELETE'
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+            })
+    }
+
     return (
         <>
-            <div className="card max-w-96 bg-base-100 shadow-xl mx-auto mb-4">
+            <div className="card w-96 bg-base-100 shadow-xl mx-auto mb-4">
                 <figure><img src={image} alt={tourist_spot_name} /></figure>
                 <div className="card-body">
                     <h2 className="card-title">{tourist_spot_name}</h2>
-                    <p>{country_name}</p>
+                    <p>Country: {country_name}</p>
+                    <p>Spot Id: {usersSpot._id}</p>
+                    <p>Cost: {average_cost}</p>
+                    <p>Short Description: {short_description}</p>
                     <div className="card-actions justify-end">
-                        <button className="btn btn-primary">View Details</button>
+                        <button onClick={() => handleDelete(usersSpot._id)} className="btn btn-primary">Delete</button>
                     </div>
                 </div>
             </div>
--- src/provider/TouristSpotsProveder.jsx
+++ src/provider/TouristSpotsProveder.jsx
@@ -3,9 +3,8 @@ import React, { createContext } from 'react';
 export const TouristSpotsContext = createContext(null)
 
 const TouristSpotsProvider = ({ children }) => {
-    // const serverLink = 'http://localhost:5000';
-    // const serverLink = 'https://tourism-management-project-6m7e6ruub-mh-mitas-projects.vercel.app/tourist-spots';
-    const serverLink = 'https://tourism-management-project.vercel.app';
+    const serverLink = 'http://localhost:5000';
+    // const serverLink = 'https://tourism-management-project.vercel.app';
     const spotsInfo = { serverLink }
     return (
         <TouristSpotsContext.Provider value={spotsInfo}>
