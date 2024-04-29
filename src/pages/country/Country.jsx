fa888dd8d14c97f6ec4639e596288f1df35d459f
Mh Mitas <mahfuzulmitas@gmail.com>
2024-04-29 15:37:45 +0600

    coundry data displayed

--- src/pages/country/Country.jsx
+++ src/pages/country/Country.jsx
@@ -5,24 +5,21 @@ const Country = () => {
     const { serverLink } = useContext(TouristSpotsContext)
     const [countries, setCountries] = useState([])
     useEffect(() => {
-        //fetch(`${serverLink}/tourist-spots/countries/all`)
-        // .then(res => res.json())
-        // .then(data => {
-        //     console.log(data);
-        // })
+        fetch(`${serverLink}/tourist-spots/allCountries`)
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                setCountries(data)
+            })
     }, [])
 
     return (
         <div className='my-10'>
             <h3 className='text-2xl py-6 font-bold'>Best Countries to visit in South Aisa</h3>
-            <div className='grid grid-cols-2 md:grid-cols-3 xl:grid-cols-5 lg:grid-cols-4 gap-6'>
-                <CountryCard></CountryCard>
-                <CountryCard></CountryCard>
-                <CountryCard></CountryCard>
-                <CountryCard></CountryCard>
-                <CountryCard></CountryCard>
-                <CountryCard></CountryCard>
-
+            <div className='grid grid-cols-2 md:grid-cols-3 xl:grid-cols-6 lg:grid-cols-4 gap-6'>
+                {
+                    countries.map(country => <CountryCard key={country._id} countryData={country}></CountryCard>)
+                }
             </div>
 
         </div>
@@ -31,14 +28,14 @@ const Country = () => {
 
 export default Country;
 
-function CountryCard() {
+function CountryCard({ countryData }) {
     return (
         <div className="max-w-96 shadow-md hover:shadow-2xl bg-base-100 rounded-sm cursor-pointer hover:scale-105">
-            <img src="https://encrypted-tbn3.gstatic.com/licensed-image?q=tbn:ANd9GcSjuLg9doPpdcH2F4H5w7LewkDCG_IfuQyfL8BvaDpGT9Op43j68wxaDSr-w28cb_1PeIaPEb-RqAjjLZZ0Vkrp1992mXWtfh-Zu11jSQ" alt="" className="w-full" />
+            <img src={countryData.image} alt="" className="w-full " />
             <div className="flex flex-col justify-between p-6">
                 <div className="">
-                    <h2 className="text-xl font-bold tracking-wide my-1">Malysia</h2>
-                    <p className="">Curabitur luctus erat nunc, sed ullamcorper erat vestibulum eget.</p>
+                    <h2 className="text-xl font-bold tracking-wide my-1">{countryData.country_name}</h2>
+                    <p className="">{countryData.description.slice(0, 95)}</p>
                 </div>
             </div>
         </div>
